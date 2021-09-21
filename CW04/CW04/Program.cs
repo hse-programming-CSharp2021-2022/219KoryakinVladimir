0846dd88a5fb010b682e2e2c6c2da8e2c4ad1f89
unknown <footbolistvova@mail.ru>
2021-09-21 21:22:00 +0300

    CW04

---
+++ CW04/CW04/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CW04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double delta = double.Parse(Console.ReadLine());
+            double integral = 0;
+            while (a + delta <= b)
+            {
+                integral += delta * (a * a + (a + delta) * (a + delta)) / 2;
+                a += delta;
+            }
+
+            a -= delta;
+            integral += (b - a) * (a * a + b * b) / 2;
+            Console.WriteLine(integral);
+        }
+    }
+}
---
+++ CW04/Task02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task02
+{
+    class Program
+    {
+        static int A(int m, int n)
+        {
+            if (m == 0)
+            {
+                return n + 1;
+            }
+            else if (m > 0 && n == 0)
+            {
+                return A(m - 1, 1);
+            }
+            else if (m > 0 && n > 0)
+            {
+                return A(m - 1, A(m, n - 1));
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(A(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+        }
+    }
+}
