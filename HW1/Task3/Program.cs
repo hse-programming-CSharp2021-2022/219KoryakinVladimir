f3d7a14a9dbdea0112046062d2bfde7ac2ad9182
VladimirKoryakin <51477015+VladimirKoryakin@users.noreply.github.com>
2022-03-26 10:01:28 +0300

    Add files via upload

---
+++ HW1/Task3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task3
+{
+    delegate double DelegateConvertTemperature(double temperature);
+    static class TemperatureConverterImp
+    {
+        public static double FromFahrenheit(double f) => (f - 32) * 5 / 9;
+        public static double ToFahrenheit(double c) => c * 9 / 5 + 32;
+    }
+    static class StaticTempConverters
+    {
+        public static double CToK(double c) => c + 273.15;
+        public static double CFromK(double k) => k - 273.15;
+        
+        public static double CToR(double c) => (c + 273.15) * 9 / 5;
+        public static double CFromR(double r) => (r - 491.67) * 5 / 9;
+        
+        public static double ToFa(double c) => c * 4 / 5;
+        public static double FromReomur(double re) => re * 5 / 4;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelegateConvertTemperature temp1 = new DelegateConvertTemperature(TemperatureConverterImp.FromFahrenheit);
+            DelegateConvertTemperature temp2 = new DelegateConvertTemperature(TemperatureConverterImp.ToFahrenheit);
+            DelegateConvertTemperature temp3 = new DelegateConvertTemperature(StaticTempConverters.CToK);
+            DelegateConvertTemperature temp4 = new DelegateConvertTemperature(StaticTempConverters.CFromR);
+            DelegateConvertTemperature temp5 = new DelegateConvertTemperature(StaticTempConverters.CToR);
+            
+            Console.WriteLine(temp1(123));
+            Console.WriteLine(temp2(0.5));
+            
+            DelegateConvertTemperature[] dels = new DelegateConvertTemperature[] {temp2, temp3, temp4, temp5};
+
+            int c = int.Parse(Console.ReadLine());
+            
+            foreach (var del in dels)
+            {
+                Console.WriteLine(del.Method.Name + " - " + del(c));
+            }
+        }
+    }
+}
---
+++ HW2/Task6/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Task6
+{
+    class Plant
+    {
+        private double _growth;
+        private double _photosensitivity;
+        private double _frostresistance;
+
+        public double Growth { get => _growth; set {_growth = value;} }
+        public double Photosensitivity { get => _photosensitivity; set { _photosensitivity = Math.Clamp(value, 0, 100); } }
+        public double Frostresistance { get => _frostresistance; set { _frostresistance = Math.Clamp(value, 0, 100); } }
+
+        public Plant(double g, double p, double f)
+        {
+            Growth = g;
+            Photosensitivity = p;
+            Frostresistance = f;
+        }
+
+        public override string ToString()
+        {
+            return $"{Growth:f2} {Photosensitivity:f2} {Frostresistance:f2}";
+        }
+    }
+
+    class Program
+    {
+        public static int SortByPhsens(Plant a, Plant b)
+        {
+            if (a.Photosensitivity % 2 == 1 && b.Photosensitivity % 2 == 0) return 1;
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            Random rand = new();
+
+            int n = int.Parse(Console.ReadLine());
+            Plant[] plants = new Plant[n];
+            for (int i = 0; i < n; i++)
+            {
+                plants[i] = new(rand.NextDouble() * 75 + 25, rand.NextDouble() * 100, rand.NextDouble() * 80);
+            }
+
+            Array.ForEach(plants, (t) => Console.WriteLine(t));
+            Console.WriteLine();
+
+            Array.Sort(plants, delegate (Plant a, Plant b)
+            {
+                if (a.Growth < b.Growth) return 1;
+                return -1;
+            });
+            Array.ForEach(plants, (t) => Console.WriteLine(t));
+            Console.WriteLine();
+
+            Array.Sort(plants, (a, b) =>
+            {
+                if (a.Frostresistance > b.Frostresistance) return 1;
+                return -1;
+            });
+            Array.ForEach(plants, (t) => Console.WriteLine(t));
+            Console.WriteLine();
+
+            Array.Sort(plants, SortByPhsens);
+            Array.ForEach(plants, (t) => Console.WriteLine(t));
+            Console.WriteLine();
+
+            Array.ConvertAll(plants, (t) => 
+            { 
+                if (((int)t.Frostresistance) % 2 == 0)
+                {
+                    t.Frostresistance /= 3;
+                } else
+                {
+                    t.Frostresistance /= 2;
+                }
+                return t;
+            });
+            Array.ForEach(plants, (t) => Console.WriteLine(t));
+            Console.WriteLine();
+        }
+    }
+}
